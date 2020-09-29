2cf673dcd919effe44ab59dee93a9eec383dcc1c
guilhermeemanuell <emanuellguilherme@gmail.com>
2020-09-29 16:31:06 -0300

    entregando incompleto pois não estou compreendo muito bem a matéria, parei para estudar novamente.

---
+++ semana3/operadores/index.js
@@ -0,0 +1,15 @@
+/* Exercícios de interpretação de código 
+
+1 - 
+comparador && entre true e false dá false;
+comparador && entre true, false, true, dá false;
+a exclamação inverte o resultado, então nessa sequência ficará com true, true, true, o que dá true nesses parâmetros;
+e o typeof é boolean porque é analisado somente true e false; 
+
+2 - 
+undefined;
+null;
+11 (número de itens dentro do parênteses);
+3 (primeiro número da sequência);
+o "primeiro" item da sequência +1 (ou seja, o próximo), será substituido por 19; */
+ 

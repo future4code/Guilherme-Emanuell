993650a8266fc7d2b28d9ee786c35866a9422778
guilhermeemanuell <emanuellguilherme@gmail.com>
2020-10-05 16:58:34 -0300

    mais um exercício de javascript incompleto. estou bem frustrado já.

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,17 @@
+/* Exercícios de interpretação de código
+
+EXERCÍCIO 1 
+a) Aparecerá os valores das duas multiplicações. Sendo uma para 2*5 e a outra 10*5. 
+b) Daria um erro pois o "console.log()" se faz necessário para que a função ocorra e seja armazenado algo.
+
+EXERCÍCIO 2
+a) Darvas e Caio pois na função informa que começará no índice 0 mas percorrerá somente até menor
+que o índice 2.
+b) Consideraria a "mesma ordem" anterior, só que inserindo a "Amanda" no lugar do "Darvas".
+
+EXERCÍCIO 3
+Não compreendi nem lendo as perguntas de outros colegas no canal de dúvidas.
+
+Exercícios de escrita de código
+EXERCÍCIO 4
+*/

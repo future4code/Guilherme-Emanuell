e7ee83171902c568a25649020809fa53e7c3a5fc
guilhermeemanuell <emanuellguilherme@gmail.com>
2020-10-06 17:01:15 -0300

    entrega incompleta

---
+++ semana4/planner/index.js
@@ -0,0 +1,9 @@
+function adicionarProduto() {
+    const tarefa = document.getElementById("descTarefa").value
+    
+    /* tarefa.value = "" */
+   /*  minhaLista.innerHTML += `<li>${meuInput.value}</li>` */
+    
+    
+    console.log(tarefa)
+}

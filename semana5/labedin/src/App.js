b3b157c10703ff0b10372009556641c48367afdf
guilhermeemanuell <emanuellguilherme@gmail.com>
2020-10-14 17:24:33 -0300

    primeira entrega do dia, com exercício incompleto.

---
+++ semana5/labedin/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import avatar from './img/avatar-guilhermee.png';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem={avatar} 
+          nome="Guilherme Emanuell" 
+          descricao="Oi! Eu sou o Guilherme, Desenvolvedor Web Full Stack graças à Labenu! ;) Por aqui você verá os meus projetos mais recentes e poderá conhecer um pouco mais da minha jornada!"
+        />
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Labenu" 
+          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        />
+        
+        <CardGrande 
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+          nome="NASA" 
+          descricao="Tester de aplicações." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+    </div>
+  );
+}
+
+export default App;

8b03a4517cbb1c80f91b2f69d5d05178113682b4
guilhermeemanuell <emanuellguilherme@gmail.com>
2020-10-27 19:22:22 -0300

    não sei nem o que dizer

---
+++ semana7/aula25/integracoesapi/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
+import BoxCadastro from "./components/cadastro"
+
+
+
+
+
+function App() {
+  return (
+    <BoxCadastro/>
+
+
+
+  );
+}
+
+export default App;

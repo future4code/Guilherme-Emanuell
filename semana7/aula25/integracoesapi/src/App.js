a9f6b7a0a847b52893a5979c9863f3e9872391d5
guilhermeemanuell <emanuellguilherme@gmail.com>
2020-10-28 21:55:39 -0300

    atualizando os arquivos

--- semana7/aula25/integracoesapi/src/App.js
+++ semana7/aula25/integracoesapi/src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
 import styled from "styled-components";
 import axios from "axios";
-import BoxCadastro from "./components/cadastro"
+import FormPage from "./components/FormPage";
+import UserListPage from "./components/UserListPage";
 
+class App extends React.Component {
+  state = {
+    formPage: true
+  }
 
+changePage = () => {
+  this.setState({formPage: !this.state.formPage})
+}  
 
+  render() {
+    const currentPage = this.state.formPage ? (<FormPage/>) : (<UserListPage/>)
 
+    return ( 
+    <div className="App">  
+      {currentPage}
+      <button onClick={this.changePage}>Mudar de Página</button>
+    </div>
 
-function App() {
-  return (
-    <BoxCadastro/>
-
-
-
-  );
+    );
+  }
 }
 
 export default App;
---
+++ semana7/aula25/integracoesapi/src/components/FormPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class FormPage extends React.Component {
+  render () {
+    return (
+      <div>
+        FormPage
+      </div>
+    )
+  }
+}
+
+export default FormPage;
+
